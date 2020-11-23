0e677e9b6c4566992f21f3ec1ca2cea9b7ed3193
SmilingCobra <1641530151@qq.com>
2020-11-23 10:28:02 +0800

    first commit

--- src/main/java/com/lw/Main.java
+++ src/main/java/com/lw/Main.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Main{
     //http://localhost:8080/test/hello
+    //git push -u origin liwei -f
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
     }
